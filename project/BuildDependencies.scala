80f112db2bfa0294145256db4b4b4097dfa35d89
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2023-07-10 14:26:37 +0100

    DDCNL-7509: Updated bootstrap and mongo

--- project/BuildDependencies.scala
+++ project/BuildDependencies.scala
@@ -3,16 +3,17 @@ import play.sbt.PlayImport._
 
 private object BuildDependencies {
   private val playVersion = "play-28"
-  private val hmrcMongoVersion = "1.1.0"
+  private val hmrcMongoVersion = "1.3.0"
+  private val bootstrapVersion = "7.19.0"
 
   val compile: Seq[ModuleID] = Seq(ehcache,
-    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"   % "7.13.0",
+    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"   % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"           % hmrcMongoVersion,
     "uk.gov.hmrc"       %% s"internal-auth-client-$playVersion" % "1.6.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"   % "7.13.0",
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"   % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion"  % hmrcMongoVersion,
     "org.mockito"       %% "mockito-scala-scalatest"        % "1.17.12"
   ).map(_ % "test, it")
