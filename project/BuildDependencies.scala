acabde49372374c286db94fd661e70f63422e6be
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2023-12-12 14:59:26 +0000

    minor updates

--- project/BuildDependencies.scala
+++ project/BuildDependencies.scala
@@ -3,19 +3,19 @@ import sbt.*
 
 private object BuildDependencies {
   private val playVersion28 = "play-28"
-  private val hmrcMongoVersion = "1.5.0"
+  private val hmrcMongoVersion = "1.6.0"
   private val bootstrapVersion28 = "7.23.0"
 
   private val playVersion29 = "play-29"
-  private val bootstrapVersion29 = "8.0.0"
+  private val bootstrapVersion29 = "8.2.0"
 
   private val playVersion30 = "play-30"
-  private val bootstrapVersion30 = "8.0.0"
+  private val bootstrapVersion30 = "8.2.0"
 
   val compile28: Seq[ModuleID] = Seq(caffeine,
     "uk.gov.hmrc"            %% s"bootstrap-frontend-$playVersion28"   % bootstrapVersion28,
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-$playVersion28"           % hmrcMongoVersion,
-    "uk.gov.hmrc"            %% s"internal-auth-client-$playVersion28" % "1.7.0",
+    "uk.gov.hmrc"            %% s"internal-auth-client-$playVersion28" % "1.9.0",
     "org.scala-lang.modules" %% "scala-collection-compat"            % "2.11.0"
   )
 
@@ -28,7 +28,7 @@ private object BuildDependencies {
   val compile29: Seq[ModuleID] = Seq(caffeine,
     "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion29" % bootstrapVersion29,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion29" % hmrcMongoVersion,
-    "uk.gov.hmrc" %% s"internal-auth-client-$playVersion29" % "1.7.0",
+    "uk.gov.hmrc" %% s"internal-auth-client-$playVersion29" % "1.9.0",
     "org.scala-lang.modules" %% "scala-collection-compat" % "2.11.0"
   )
 
@@ -41,7 +41,7 @@ private object BuildDependencies {
   val compile30: Seq[ModuleID] = Seq(caffeine,
     "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion30" % bootstrapVersion30,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion30" % hmrcMongoVersion,
-    "uk.gov.hmrc" %% s"internal-auth-client-$playVersion30" % "1.8.0",
+    "uk.gov.hmrc" %% s"internal-auth-client-$playVersion30" % "1.9.0",
     "org.scala-lang.modules" %% "scala-collection-compat" % "2.11.0"
   )
 
